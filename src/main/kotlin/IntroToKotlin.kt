fb4dfadc55114ef0fa5016c87b7290f999b74923
BEYZANUR AKKUZU <61544442+beyzanurakkuzu@users.noreply.github.com>
2022-06-12 20:28:20 +0300

    first commit

---
+++ src/main/kotlin/IntroToKotlin.kt
@@ -0,0 +1,46 @@
+// package --> dosyanın hangi klasör içerisinde olduğunu söyler
+
+//import --> harici olarak kullnılan kütüphanelerin nereden alındığını söyler ve içeriye aktarmayı sağlar.
+
+
+fun main(){
+    // jvm projelerinin ana giriş noktasıdır -> "main"
+    // özel isimdir çift kullanılamaz
+
+
+    print("Hello, ")
+    println("Welcome to Kotlin intro classes")
+    print("01-06-2022")
+
+    //print -> çıktıları yan yana yazdırır
+    //println -> alt satıra geçirir
+    /* Run -> Console Çıktısı:
+    *  Hello, Welcome to kotlin intro classes
+    *  01-06-2022
+    *
+    * */
+}
+
+
+/*
+fun main(args:Array<String>){
+args alan main fonksiyonu
+    * Run -> Edit Configurations -> Program Arguments -> tr,en,fr -->
+    args: Array içerisine bu değerleri bu şekilde vermiş oluruz
+}
+ */
+
+// tekli komut satırı
+
+/*
+Çoklu
+komut
+satırı
+ */
+
+/**
+ * Parametreli komut satırı @author Beyza
+
+ */
+
+//TODO buraya yeni bilgiler eklenecek
